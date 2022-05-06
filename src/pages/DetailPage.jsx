1ff23a4d48de9794faaccf4e6dbff09b3290df7a
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-05-06 23:54:55 +0200

    detail

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React,{useContext, useEffect, useState}from 'react'
+import { ProductContect } from '../context/Product'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Button,Card } from 'react-bootstrap'
 
 const DetailPage = () => {
+
+const {product} = useContext(ProductContect)
+const {id} =useParams()
+const [data ,setData]= useState()
+const navi =useNavigate()
+
+
+useEffect(()=>{
+
+  const currentdata = product.filter(el => el.id === id)
+  setData(currentdata[0])
+},[])
+
+
   return (
-    <div>DetailPage</div>
+    <div>
+      <Button onClick={()=>navi('/')}>Go to Product</Button>
+      <Card style={{width:'40rem',height:'4orem',margin:'100px auto'}}>
+        <Card.Img  variant='top' src='/frut.jpg'/>
+
+
+      </Card>
+
+
+
+    </div>
   )
 }
 
