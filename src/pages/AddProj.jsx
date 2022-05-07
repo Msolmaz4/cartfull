10dae3c9d364dc9511542b4242c1b266734afe30
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-05-07 23:23:58 +0200

    Add

--- src/pages/AddProj.jsx
+++ src/pages/AddProj.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import  {Form,InputGroup,FormControl,Button, FormSelect} from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+
+
 
 const AddProj = () => {
+
+const navi =useNavigate()
+
   return (
-    <div>AddProj</div>
+    <div>
+      <Button onClick={()=>navi('/')}>Goto </Button>
+      <InputGroup className='w-75' style={{margin:'150px  auto'}}>
+      <InputGroup.Text>Name</InputGroup.Text>
+      <FormControl name='name' type='text' />
+      <InputGroup.Text>Price</InputGroup.Text>
+      <FormControl name='price' type='number' />
+      <FormSelect name='category' style={{width:'20%'}}>
+        <option >Select menu</option>
+        <option value='fruit'>Fruit</option>
+        <option value='vege'>Vege</option>
+        <option value='dairy'>Dairy</option>
+
+      </FormSelect>
+      </InputGroup>
+
+
+    </div>
   )
 }
 
